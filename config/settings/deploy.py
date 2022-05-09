d8bbac913ecf39b7b14a4a27d4ad6581d029c60f
osawldeff <tialem94@gmail.com>
2022-05-09 23:57:58 +0900

    first commit

---
+++ config/settings/deploy.py
@@ -0,0 +1,22 @@
+from .base import *
+
+
+DEBUG = False
+
+
+ALLOWED_HOSTS += [
+    
+]
+
+
+DATABASES = {
+    'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'OPTIONS': {'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'},
+            'HOST': os.environ.get('DATABASES_HOST'),
+            'NAME': os.environ.get('DATABASES_NAME'),
+            'USER': os.environ.get('DATABASES_USER'),
+            'PASSWORD': os.environ.get('DATABASES_PASSWORD'),
+            'PORT': os.environ.get('DATABASES_PORT'),
+        }
+}
---
+++ config/settings/dev.py
@@ -0,0 +1,12 @@
+from .base import *
+
+
+ALLOWED_HOSTS += [
+    '127.0.0.1', 
+    'localhost',
+]
+
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
