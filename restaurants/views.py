35a5c8b0a6562d006ab00b810b8d5ac5c8c189ec
osawldeff <tialem94@gmail.com>
2022-05-16 16:14:43 +0900

    feat: Update view and template realted to category queryset

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.http import HttpRequest
+from django.core.paginator import Paginator
+from .models import MainCategory, Restaurant
 import os
 
 
@@ -11,6 +14,24 @@ def home(request: HttpRequest):
     context = {
         'NAVER_MAP_CLIENT_ID': os.environ.get('NAVER_MAP_CLIENT_ID'),
         'LOCAL_HOST': os.environ.get('LOCAL_HOST'),
-        'PAGE': 'home'
+        'SECTION': 'home'
     }
+    q = Q()
+    
+    if request.GET.get('search', ''):
+        search_keyword = request.GET.get('search', '')
+        queryset = Restaurant.objects.all().order_by('distance')[:5]
+        context['RESTAURANTS'] = queryset
+    
+    if request.GET.get('category', ''):
+        category = request.GET.get('category', '')
+        page = request.GET.get('page', '1')
+        context['CATEGORY'] = category
+        queryset = Restaurant.objects.select_related('main_category').filter(main_category__name=category).order_by('distance')
+        paginator = Paginator(queryset, 3)
+        queryset = paginator.get_page(page)
+        context['CATEGORY'] = category
+        context['PAGE'] = page
+        context['RESTAURANTS'] = queryset
+    
     return render(request, template_name, context)
