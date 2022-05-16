a4c7312013b7167dc32ebc276813c402b3271dfd
osawldeff <tialem94@gmail.com>
2022-05-16 22:42:47 +0900

    feat: Add img field and Update naver_rating field in restaurant model

--- restaurants/models.py
+++ restaurants/models.py
@@ -24,10 +24,11 @@ class Restaurant(TimeStampedModel):
     near_station = models.CharField(max_length=100, verbose_name='인근 역')
     name = models.CharField(max_length=100, unique=True, verbose_name='이름')
     address = models.CharField(max_length=255, verbose_name='주소')
+    img = models.URLField(blank=True, null=True, verbose_name='이미지')
     latitude = models.DecimalField(max_digits=10, decimal_places=7, null=True, default=None, verbose_name='위도')
     longitude = models.DecimalField(max_digits=10, decimal_places=7, null=True, default=None, verbose_name='경도')
     distance = models.DecimalField(max_digits=6, decimal_places=2, null=True, default=None, verbose_name='거리')
-    naver_rating = models.DecimalField(max_digits=2, decimal_places=1, null=True, default=None, verbose_name='네이버 별점')
+    naver_rating = models.DecimalField(max_digits=2, decimal_places=1, default=0, verbose_name='네이버 별점')
     search_count = models.PositiveIntegerField(default=0, verbose_name='검색 카운트')
     is_delete = models.BooleanField(default=False, verbose_name='삭제')
     
