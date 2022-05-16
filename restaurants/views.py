c31d749f2aee2699586afa3d0517ca4258e3f8ba
osawldeff <tialem94@gmail.com>
2022-05-16 21:53:37 +0900

    feat: Update context key and value using dict update

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.core.paginator import Paginator
 from .models import MainCategory, Restaurant
 import os
+import re
 
 
 # Create your views here.
@@ -15,17 +16,35 @@ def home(request: HttpRequest):
         'NAVER_MAP_CLIENT_ID': os.environ.get('NAVER_MAP_CLIENT_ID'),
         'LOCAL_HOST': os.environ.get('LOCAL_HOST'),
         'SECTION': 'home',
-        'AUTH': True
+        'AUTH': False,
+        'CATEGORY': False,
+        'SEARCH_KEYWORD': False,
+        'ORDER_BY': False
     }
     q = Q()
     
     if request.GET.get('search', ''):
         search_keyword = request.GET.get('search', '')
-        queryset = Restaurant.objects.all().order_by('distance')[:5]
-        context['RESTAURANTS'] = queryset
+        search_keyword = search_keyword.replace(' ', '')
+        search_keyword = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '', search_keyword)
+        
+        page = request.GET.get('page', '1')
+        queryset = Restaurant \
+            .objects \
+            .filter(sub_category__icontains=search_keyword) \
+            .order_by('distance')
+        paginator = Paginator(queryset, 5)
+        queryset = paginator.get_page(page)
+        
+        context.update({
+            'SEARCH_KEYWORD': search_keyword,
+            'PAGE': page,
+            'RESTAURANTS': queryset
+        })
     
     if request.GET.get('category', ''):
         category = request.GET.get('category', '')
+        
         page = request.GET.get('page', '1')
         queryset = Restaurant \
             .objects \
@@ -34,8 +53,11 @@ def home(request: HttpRequest):
             .order_by('distance')
         paginator = Paginator(queryset, 5)
         queryset = paginator.get_page(page)
-        context['CATEGORY'] = category
-        context['PAGE'] = page
-        context['RESTAURANTS'] = queryset
+        
+        context.update({
+            'CATEGORY': category,
+            'PAGE': page,
+            'RESTAURANTS': queryset
+        })
     
     return render(request, template_name, context)
