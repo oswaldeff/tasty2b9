e9bb2d9dc11c219d7f7a0239d5621f9bda5c1a2f
osawldeff <tialem94@gmail.com>
2022-05-18 17:57:58 +0900

    feat: Update request type

--- core/views.py
+++ core/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpRequest, HttpResponseRedirect
 from django.urls import reverse
 
 
 # Create your views here.
 
 
-def proxy_home(request):
+def proxy_home(request: HttpRequest):
     return HttpResponseRedirect(reverse('restaurants:home'))
