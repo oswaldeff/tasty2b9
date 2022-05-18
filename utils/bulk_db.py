7116b8de7fcfa58bffc3c599bc9f657d3b6af3b5
osawldeff <tialem94@gmail.com>
2022-05-19 04:24:05 +0900

    feat: Add meal data

--- utils/bulk_db.py
+++ utils/bulk_db.py
@@ -1,7 +1,8 @@
+from tkinter import Menu
 from django.http import HttpResponse, HttpRequest
 from .scraper import get_restaurant_main_info
 from .naver_api import get_restaurant_sub_info, get_restaurant_lat_lng
-from restaurants.models import MainCategory, Restaurant
+from restaurants.models import MainCategory, Restaurant, Menu
 import math
 import random
 import requests
@@ -25,7 +26,6 @@ def gen_restaurant_data(request: HttpRequest):
     restaurant_databases = []
     main_info_content = get_restaurant_main_info()
     for main_info in main_info_content:
-        main_category_database = None
         restaurant_database = {
             'main_category': None,
             'sub_category': None,
@@ -59,14 +59,16 @@ def gen_restaurant_data(request: HttpRequest):
         elif main_info['main_category'] == '양식':
             main_info['main_category'] = western_style_food
         
-        restaurant_database['main_category'] = main_info['main_category']
-        restaurant_database['sub_category'] = sub_info_content['sub_category']
-        restaurant_database['near_station'] = main_info['near_station']
-        restaurant_database['name'] = main_info['name']
-        restaurant_database['address'] = sub_info_content['address']
-        restaurant_database['latitude'] = latitude
-        restaurant_database['longitude'] = longitude
-        restaurant_database['distance'] = distance
+        restaurant_database.update({
+            'main_category': main_info['main_category'],
+            'sub_category': sub_info_content['sub_category'],
+            'near_station': main_info['near_station'],
+            'name': main_info['name'],
+            'address': sub_info_content['address'],
+            'latitude': latitude,
+            'longitude': longitude,
+            'distance': distance
+        })
         restaurant_databases.append(restaurant_database)
     
     Restaurant.objects.bulk_create([Restaurant(**data) for data in restaurant_databases], ignore_conflicts=True)
@@ -111,4 +113,67 @@ def update_restaurant_data(request: HttpRequest):
             restaurant.save()
         except:
             pass
+    return HttpResponse(status=200)
+
+
+# generate menu data
+def gen_menu_data(request: HttpRequest):
+    header = {
+        'authority': 'map.naver.com',
+        'method': 'GET',
+        'scheme': 'https',
+        'accept': 'application/json, text/plain, */*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
+        'cache-control': 'no-cache',
+        'content-type': 'application/json',
+        'cookie': 'NNB=ZHREYBKYJGZWA; ASID=7c6f74d40000017a324f783a0000004f; NV_WETR_LOCATION_RGN_M="MDkxNDAxMDQ="; _ga=GA1.1.843260892.1631082249; _ga_8P4PY65YZ2=GS1.1.1631546146.1.0.1631546150.0; NV_WETR_LAST_ACCESS_RGN_M="MDkxNDAxMDQ="; MM_NEW=1; NFS=2; NaverSuggestUse=use%26unuse; _gcl_au=1.1.684846510.1636646068; nx_ssl=2; page_uid=4bf320b5-0774-4667-9eca-95099ebb77e8',
+        'expires': 'Sat, 01 Jan 2000 00:00:00 GMT',
+        'pragma': 'no-cache',
+        'referer': 'https://map.naver.com/',
+        'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': "Windows",
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
+    }
+    
+    menu_databases = []
+    for restaurant in Restaurant.objects.all():
+        
+        url = f'https://map.naver.com/v5/api/search?caller=pcweb&query=강남%20{restaurant.name}'
+        try:
+            response = requests.get(url, headers=header)
+            content_json = json.loads(response.content)['result']['place']['list'][0]
+            time.sleep(2)
+            menu_info = content_json['menuInfo']
+            menu_price_sets = menu_info.split(' | ')
+            
+            cnt = 0
+            for menu_price_set in menu_price_sets:
+                menu_database = {
+                    'restaurant': restaurant,
+                    'name': None,
+                    'price': None
+                }
+                menu_price_set = menu_price_set.rsplit(' ', 1)
+                menu_name = menu_price_set[0]
+                menu_price = int(menu_price_set[1].replace(',', ''))
+                menu_database.update({
+                    'name': menu_name,
+                    'price': menu_price
+                })
+                menu_databases.append(menu_database)
+                cnt += 1
+        except:
+            if cnt == 0:
+                menu_databases.append({
+                        'restaurant': restaurant,
+                        'name': '메뉴없음',
+                        'price': 0
+                    })
+    
+    Menu.objects.bulk_create([Menu(**data) for data in menu_databases], ignore_conflicts=True)
     return HttpResponse(status=200)
--- utils/urls.py
+++ utils/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .bulk_db import gen_restaurant_data, update_restaurant_data
+from .bulk_db import gen_restaurant_data, update_restaurant_data, gen_menu_data
 
 
 urlpatterns = [
     path('/restaurants/database', gen_restaurant_data, name='restaurant_database'),
     path('/restaurants/star/img', update_restaurant_data, name='restaurant_star_img'),
+    path('/meals/database', gen_menu_data, name='meal_database'),
 ]
