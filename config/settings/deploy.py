cc5ad58031aa7f22fc8791f9d86d8312ff4d5e2c
osawldeff <tialem94@gmail.com>
2022-05-19 00:44:38 +0900

    feat: Update DATABASES setting for emozi

--- config/settings/deploy.py
+++ config/settings/deploy.py
@@ -12,7 +12,10 @@ ALLOWED_HOSTS += [
 DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'OPTIONS': {'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'},
+            'OPTIONS': {
+                'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
+                'charset': 'utf8mb4',
+            },
             'HOST': os.environ.get('DATABASES_HOST'),
             'NAME': os.environ.get('DATABASES_NAME'),
             'USER': os.environ.get('DATABASES_USER'),
