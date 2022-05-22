f1595f027c7294c1fe9a32670408d21ab8ec47c5
osawldeff <tialem94@gmail.com>
2022-05-23 00:23:20 +0900

    feat:

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -13,22 +13,3 @@ INTERNAL_IPS = [
     '127.0.0.1',
     os.environ.get('LOCAL_HOST'),
 ]
-
-import pymysql
-# pymysql setting
-pymysql.version_info = (1, 4, 2, "final", 0)
-pymysql.install_as_MySQLdb()
-DATABASES = {
-    'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'OPTIONS': {
-                'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
-                'charset': 'utf8mb4',
-            },
-            'HOST': os.environ.get('DATABASES_HOST'),
-            'NAME': os.environ.get('DATABASES_NAME'),
-            'USER': os.environ.get('DATABASES_USER'),
-            'PASSWORD': os.environ.get('DATABASES_PASSWORD'),
-            'PORT': os.environ.get('DATABASES_PORT'),
-        }
-}
